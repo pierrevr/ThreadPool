08226cf9597208d88a18c20689e5f28ed2deac40
Pierre Vorhagen <pvorhagen@gmail.com>
2012-01-09 17:45:14 +0100

    licensing and readme update

--- ThreadPool.cpp
+++ ThreadPool.cpp
@@ -1,4 +1,21 @@
-﻿#include "ThreadPool.hpp"
+﻿/*
+ * Copyright 2011 Pierre Vorhagen, Etienne Doms.
+ *
+ * This library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "ThreadPool.hpp"
 
 ThreadPool::ThreadPool(unsigned int size) : _size(0), _tasks(0), _pause(false) {
     pthread_mutex_init(&_mutex_queue, NULL);
@@ -69,7 +86,7 @@ void ThreadPool::flush() {
 
 void ThreadPool::pause() {
     pthread_mutex_lock(&_mutex_pause);
-    _pause = true;  
+    _pause = true;
     pthread_mutex_unlock(&_mutex_pause);
 }
 
@@ -77,7 +94,7 @@ void ThreadPool::resume() {
     pthread_mutex_lock(&_mutex_pause);
     _pause = false;
     pthread_mutex_unlock(&_mutex_pause);
-    
+
     pthread_cond_broadcast(&_cond_pause);
 }
 
